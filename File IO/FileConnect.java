76d915caa42a696956de77339fb4747b57b2e3cd
Brad Stell <BradStell31@gmail.com>
2015-03-07 13:43:47 -0500

    adding file i/o

--- File IO/FileConnect.java
+++ File IO/FileConnect.java
@@ -109,4 +109,48 @@ public class FileConnect {
         return scan;
     }
 	
+    public static List<T> getUniqueListFromFile(String filename, T type) {
+        // Lets make sure the file path is not empty or null
+        if (filename == null || filename.isEmpty()) {
+            System.out.println("Invalid File Path");
+            return;
+        }
+        String filePath = System.getProperty("user.dir") + "/" + filename;
+        BufferedReader inputStream = null;
+        // We need a try catch block so we can handle any potential IO errors
+        try {
+            try {
+                inputStream = new BufferedReader(new FileReader(filePath));
+                String lineContent = null;
+                // Loop will iterate over each line within the file. // It will
+                // stop when no new lines are found.
+                
+                HashSet<type> userHash = new HashSet<type>();
+                List<type> itm = new ArrayList<type>();
+                while ((lineContent = inputStream.readLine()) != null) {                
+                    
+                    type t = new type(lineContent);
+                    boolean exists = userHash.add(u);
+                    if (!exists) {                      
+                        
+                        itm.add(u);
+                    }
+                    
+                }
+                Collections.sort(itm);
+                
+                return itm;
+                
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            }
+        } // Make sure we close the buffered reader.
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
